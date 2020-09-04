e1d292f317cf199bba5d5d0e7ac732cf265c5b55
rfreitasbatista <rfreitasbatista@gmail.com>
2020-09-04 17:23:18 -0300

    First Scan

--- src/components/content/InitialPage/index.js
+++ src/components/content/InitialPage/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import '../../../css/initialPage.css';
 
 class InitialPage extends Component {
-  render() {
+  render() { 
     return (
       <h1 className="title">Faça uma pesquisa!</h1>
     )
